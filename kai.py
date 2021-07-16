0dbb0c36c3819da0ba7efa3dcf8d8b3b7ca00aec
Kaylode Kieran <56054379+kaylode@users.noreply.github.com>
2021-07-16 13:28:22 +0000

    init default

---
+++ kai.py
@@ -0,0 +1,33 @@
+import discord
+import os
+import requests
+import json
+from replit import db
+from server import keep_alive
+
+client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    if "responding" not in db.keys():
+        db["responding"] = True
+
+keep_alive()
+TOKEN = os.getenv('TOKEN')
+client.run(TOKEN)
