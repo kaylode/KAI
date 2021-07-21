8d4804408a9f1dbf49b7ab464225e1ce03f80634
kaylode <pmkhoi@selab.hcmus.edu.vn>
2021-07-21 18:04:22 +0700

    add gg translation api

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,8 +1,11 @@
 from .food_api import FoodAPI
 from .dictionary import Dictionary
+from .translate_api import GoogleTranslationAPI
 
 def get_api(name):
     if name == 'dictionary':
         return Dictionary()
     if name == 'foodapi':
         return FoodAPI()
+    if name == 'translateapi':
+        return GoogleTranslationAPI()
