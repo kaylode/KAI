1b230bf6df7afaf95d9d7ed6f0d6459e2be6f981
Kaylode <kayp.kieran@gmail.com>
2021-09-18 02:28:39 +0700

    fix youtube bug

--- apis/music.py
+++ apis/music.py
@@ -22,7 +22,8 @@ ytdl_format_options = {
 }
 
 ffmpeg_options = {
-    'options': '-vn'
+    'options': '-vn',
+    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
