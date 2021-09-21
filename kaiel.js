cde28090df4eab4beae52e5b668e034cfa283884
Kaylode <kayp.kieran@gmail.com>
2021-09-21 16:34:04 +0700

    finish kaiel

--- kaiel.js
+++ kaiel.js
@@ -8,6 +8,24 @@ const discordSR = new DiscordSR(client, {
   speechRecognition: resolveSpeechWithGoogleSpeechV2,
 });
 
+const db = require('./database/db.json');
+var dict = db['voice'];
+
+function mapTriggerWords(text){
+    let result = text;
+    let tokens = text.split(" ");
+    let triggers = [tokens[0], tokens.slice(0,2).join(' ')];
+    for (const tr of triggers) {
+        if (tr in dict) {
+            console.log(tr);
+            result = dict[tr][0];
+            result = result.concat(' ', tokens.slice(2).join(' '));
+            return result;
+        }
+    }
+    return result;
+}
+
 client.once('ready', () => {
     console.log('Ready!');
 });
@@ -19,11 +37,13 @@ client.on('message', message => {
         if (message.content=='$listen on') {
             message.member.voice.channel.join();
             channel = message.channel;
+            channel.send("Vui lòng nói lớn, tao mới nghe được.");
         }
     }
 
     if (message.content=='$listen off') {
         message.member.voice.channel.leave();
+        channel.send("Tạm biệt.");
     }
 })
 
@@ -33,6 +53,7 @@ client.on('speech', message => {
     var tokens;
     try{
         response = response.toLowerCase();
+        response = mapTriggerWords(response);
         tokens = response.split(" ");
         let trigger = tokens[0];
         switch(trigger) {
@@ -41,6 +62,7 @@ client.on('speech', message => {
             case "continue":
             case "next":
             case "pause":
+            case "stop":
             case "resume":
             case "wiki":
             case "voice":
